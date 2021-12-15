0cece27ff72408b5e2a8b5650145822784c41956
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-12-15 17:01:51 +0000

    Init theme

---
+++ wp-content/themes/ledgeredge/functions.php
@@ -0,0 +1,25 @@
+<?php
+
+// Set theme defaults, tidy up WP
+require_once( 'lib/theme-setup.php' );
+
+// Load scrips
+require_once( 'lib/enqueue.php' );
+
+// Images
+require_once( 'lib/images.php' );
+
+// Configure menus
+require_once( 'lib/menus.php' );
+
+// ACF configuration
+require_once( 'lib/acf.php' );
+
+// Custom Gutenberg blocks
+require_once( 'lib/blocks.php' );
+
+// Vendor specific functions
+require_once( 'lib/vendors.php' );
+
+// Helper functions
+require_once( 'lib/helpers.php' );
