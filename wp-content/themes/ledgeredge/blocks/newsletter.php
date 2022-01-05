485e82b11680b1eb95a5003b24512887cb55beac
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-01-05 13:45:31 +0000

    Add max width

--- wp-content/themes/ledgeredge/blocks/newsletter.php
+++ wp-content/themes/ledgeredge/blocks/newsletter.php
@@ -3,7 +3,7 @@
 * The template used for displaying a newsletter.
 */
 ?>
-<section class="newsletter">
+<section class="fade newsletter">
 	<div class="container--small">
 		<p class="lead">Newsletter sign up</p>
 		<p>Stay connected to the latest news, platform, team and industry updates from LedgerEdge and our partners. </p>
