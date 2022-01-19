a1cff0622bdab1ff770f0488ce840b77e1393e97
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-01-19 12:46:24 +0000

    Add 404 redirect

--- wp-content/themes/ledgeredge/page.php
+++ wp-content/themes/ledgeredge/page.php
@@ -1,4 +1,10 @@
-<?php get_header(); ?>
+<?php
+// If 404 page return 404 header
+if(is_page('1578')):
+    header("HTTP/1.0 404 Not Found");
+endif;
+get_header();
+?>
 
 <?php while ( have_posts() ) : the_post(); ?>
 	<?php the_content(); ?>
