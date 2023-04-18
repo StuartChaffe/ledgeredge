81efdc317898befbf4ef9bb9f0e9d211a73e6d65
Stuart Chaffe <stu@stuartchaffe.co.uk>
2023-04-18 14:22:36 +0100

    Style team

--- wp-content/themes/ledgeredge/blocks/team.php
+++ wp-content/themes/ledgeredge/blocks/team.php
@@ -22,11 +22,24 @@
 		$link = get_field('team_link', get_the_ID());
 	?>
 	<div class="team-item fade">
-		<!-- <div class="team-item--image" style="background-image: url('<?php echo $image['url']; ?>')"> -->
 		<?php if ($image) { ?>
 		<div class="team-item--image">
 			<svg class="icon"><?php echo get_icon('asset-team'); ?></svg>
-			<div class="team-item--image__clip" style="background-image: url('<?php echo $image['url']; ?>">
+			<div class="hexagone">
+				<div class="team-item--image__clip" style="background-image: url('<?php echo $image['url']; ?>')">
+					<img loading="lazy" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+				</div>
+			</div>
+			<svg style="visibility: hidden; position: absolute;" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
+				<defs>
+					<filter id="round">
+						<feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />    
+						<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
+						<feComposite in="SourceGraphic" in2="goo" operator="atop"/>
+					</filter>
+				</defs>
+			</svg>
+			<div class="team-item--image__clip hide" style="background-image: url('<?php echo $image['url']; ?>')">
 				<img loading="lazy" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 			</div>
 		</div>
@@ -34,7 +47,7 @@
 		<h4><?php the_title(); ?></h4>
 		<?php if ($job) { ?><h4 class="team-item--job"><?php echo $job; ?></h4><?php } ?>
 		<?php if ($bio) { ?><div class="team-item--bio"><?php echo $bio; ?></div><?php } ?>
-		<?php if ($link) { ?><span class="btn btn--primary btn__small"><a href="<?php echo $link['url']; ?>" title="<?php echo $link['title']; ?>"><?php echo $link['title']; ?> <?php echo get_icon('arrow'); ?></a></span><?php } ?>
+		<?php if ($link) { ?><a href="<?php echo $link['url']; ?>" title="<?php echo $link['title']; ?>"><?php echo $link['title']; ?></a><?php } ?>
 	</div>
 
 	<?php endwhile; wp_reset_query(); ?>
